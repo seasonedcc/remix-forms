ca946f2ca2ba3e55e04fdd500444993cbb4f993b
Gustavo Guichard <gustavoguichard@gmail.com>
2022-07-07 16:52:00 -0300

    Adjust netlify deployment

--- apps/web/app/entry.server.tsx
+++ apps/web/app/entry.server.tsx
@@ -1,6 +1,6 @@
-import { renderToString } from 'react-dom/server'
 import type { EntryContext } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
+import { renderToString } from 'react-dom/server'
 
 export default function handleRequest(
   request: Request,
@@ -8,7 +8,7 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(
+  let markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />,
   )
 
