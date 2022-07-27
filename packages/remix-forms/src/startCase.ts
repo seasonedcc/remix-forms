ca1dd381577277ccbc74f476ea3a54898b0231a8
Gustavo Guichard <gustavoguichard@gmail.com>
2022-07-27 11:20:55 -0300

    Replace lodash's startCase function with own function

---
+++ packages/remix-forms/src/startCase.ts
@@ -0,0 +1,8 @@
+// source: https://github.com/30-seconds/30-seconds-of-code/blob/master/snippets/toTitleCase.md
+
+export default function startCase(str: string): string {
+  const matches = str.match(
+    /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g,
+  ) ?? ['']
+  return matches.map((x) => x.charAt(0).toUpperCase() + x.slice(1)).join(' ')
+}
