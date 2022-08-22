d933a1bb0739642474119e300694e714621defa5
Paul Maier <pauljustus279@gmail.com>
2022-08-22 13:07:52 +0200

    Better implementation but still without delete

--- apps/web/app/routes/examples/schemas/arrays.tsx
+++ apps/web/app/routes/examples/schemas/arrays.tsx
@@ -10,26 +10,28 @@ import Form from '~/ui/form'
 import { metaTags } from '~/helpers'
 import { makeDomainFunction } from 'remix-domains'
 import Example from '~/ui/example'
-import {useActionData} from "@remix-run/react";
+import {useActionData, useFormAction} from "@remix-run/react";
 import Label from "~/ui/label";
+import {useState} from "react";
 
 const title = 'Arrays'
 const description =
   'In this example, all sorts of array schemas are validated on the client and on the server.'
+const min = 3;
+const max = 10;
 const defaultArray = ['1', '2', '3']
 
 export const meta: MetaFunction = () => metaTags({ title, description })
 
 //
-
 const code = "import {formAction} from 'remix-forms'";
-
 //
 
 const schema = z.object({
-  stringArray: z.string().array().default(defaultArray),
-  stringArrayAdd: z.string().nullable(),
-  stringArrayRemove: z.number().int().positive().nullable(),
+  hobbies: z.string().array().min(min).max(max).default(defaultArray),
+  /*hobbiesAdd: z.string().nullable(),
+  hobbiesRemove: z.number().int().positive().nullable(),
+  finalSubmit: z.boolean().default(true),*/
 })
 
 export const loader: LoaderFunction = () => ({
@@ -37,65 +39,117 @@ export const loader: LoaderFunction = () => ({
 })
 
 const mutation = makeDomainFunction(schema)(async (values) => {
-  if (!values.stringArray) {
-    values.stringArray = []
+/*  if (!values.hobbies) {
+    values.hobbies = [];
   }
 
-  if (values.stringArrayRemove) {
-    values.stringArray.splice(values.stringArrayRemove - 1, 1)
+  if (values.hobbiesRemove) {
+    values.hobbies.splice(values.hobbiesRemove - 1, 1)
+    values.finalSubmit = false;
+    values.hobbiesRemove = 0;
   }
 
-  if (values.stringArrayAdd) {
-    values.stringArray.push(values.stringArrayAdd)
-  }
+  if (values.hobbiesAdd) {
+    values.hobbies.push(values.hobbiesAdd)
+    values.finalSubmit = false;
+    values.hobbiesAdd = '';
+  }*/
 
-  return values
+  return values;
 })
 
 export const action: ActionFunction = async ({ request }) => {
+  console.log(request);
   return formAction({ request, schema, mutation })}
 
 export default function Component() {
   const data = useActionData();
-  const stringArray = data?.stringArray || data?.values?.stringArray || defaultArray || [];
-  
+  const [formAction] = useFormAction('destroy');
+  let hobbies = data?.hobbies || data?.values?.hobbies || [];
+
+  const clamp = (num:number, min:number, max:number) => Math.min(Math.max(num, min), max);
+
   return (
     <Example title={title} description={description}>
-      <Form
-        id={'myform'}
-        schema={schema}
-        options={{
-          stringArrayRemove: stringArray.map((item:string, index:number) => {
-            return {value: index+1, name: item}
-          })
-        }}
-        hiddenFields={['stringArray']}
-      >
+      <Form schema={schema}>
         {({ Field, Errors, Button }) => (
           <>
-            <Label>Array</Label>
-            <ul className={'list-disc pl-5'}>
-              {stringArray && stringArray.map((item:string, index:number) => (
-                <li key={index}>
-                  <p>{item}</p>
-                </li>
-              ))}
-            </ul>
-            <Field name="stringArray" value={stringArray ? stringArray : []}>
-              {({ SmartInput, Errors }) => (
+            <Field name="hobbies">
+              {({ Label, Errors }) => (
                 <>
-                  <SmartInput />
+                  <Label>Hobbies</Label>
+                  {JSON.stringify(hobbies)}
+                  {(Array.from({length: clamp(hobbies.length+1, min, max)}, (_, i) => i + 1)).map(i => (
+                    <div key={i}>
+                      {hobbies[i-1] ? (
+                        <>
+                          <input
+                            className={'text-blue-500'}
+                            key={i + '-value'}
+                            value={hobbies[i-1]}
+                            readOnly={true}
+                            name={'hobbies[' + i + ']'}
+                          />
+                          <button
+                            formAction={formAction}
+                            formMethod="delete"
+                          >
+                            Delete
+                          </button>
+                        </>) : (
+                        <input
+                          className={'text-blue-500'}
+                          key={i + '-empty'}
+                          name={'hobbies[' + i + ']'}
+                        />
+                      )}
+                    </div>
+                  ))}
                   <Errors />
                 </>
               )}
             </Field>
-            <Field name="stringArrayRemove" />
-            <Field name="stringArrayAdd" />
             <Errors />
-            <Button disabled={false} />
+            <Button />
           </>
         )}
       </Form>
+      {/*<Form
+        schema={schema}
+        options={{
+          hobbiesRemove: hobbies.map((item:string, index:number) => {
+            return {value: index+1, name: item}
+          })
+        }}
+      >
+        {({ Field, Errors, Button }) => (
+          <>
+            <div>
+              <Label>Hobbies</Label>
+              <ul className={'list-disc pl-5'}>
+                {hobbies && hobbies.map((item:string, index:number) => (
+                  <li key={index}>
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ul>
+              <Field name="hobbies" value={hobbies ? hobbies : []}>
+                {({ SmartInput, Errors }) => (
+                  <>
+                    <SmartInput />
+                    <Errors />
+                  </>
+                )}
+              </Field>
+              <Field name="hobbiesRemove" />
+              <Field name="hobbiesAdd" />
+            </div>
+            <Field name="finalSubmit" value={true}/>
+            <Errors />
+            <Button />
+          </>
+        )}
+      </Form>*/}
     </Example>
   )
 }
