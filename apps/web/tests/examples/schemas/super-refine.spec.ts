af27a20c5f789097df1850bb673f75a0182a199f
Diogo Biazus <diogo@biazus.ca>
2022-10-25 14:32:06 -0400

    Add disabled JS test case

--- apps/web/tests/examples/schemas/super-refine.spec.ts
+++ apps/web/tests/examples/schemas/super-refine.spec.ts
@@ -1,8 +1,35 @@
-import { test, expect } from 'tests/setup/tests'
+import { test, testWithoutJS, expect } from 'tests/setup/tests'
 
 const route = '/examples/schemas/super-refine'
 
 test('With JS enabled', async ({ example }) => {
+  const { button, page } = example
+  const planType = example.field('planType')
+  const quantity = example.field('quantity')
+
+  await page.goto(route)
+
+  // Render
+  await example.expectSelect(planType, { value: 'personal' })
+  await example.expectField(quantity, {
+    value: '',
+    required: true,
+  })
+
+  // fill
+  await quantity.input.fill('1')
+
+  // Submit
+  await expect(button).toBeEnabled()
+  await button.click()
+
+  await example.expectData({
+    planType: 'personal',
+    quantity: 1,
+  })
+})
+
+testWithoutJS('With JS disabled', async ({ example }) => {
   const { button, page } = example
   const planType = example.field('planType')
   const quantity = example.field('quantity')
