99a53aeece8ebe499878f33c125b656153ecc490
Diogo Biazus <diogo@biazus.ca>
2022-10-25 11:33:12 -0400

    Add a smoke test to super-refine case

---
+++ apps/web/tests/examples/schemas/super-refine.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from 'tests/setup/tests'
+
+const route = '/examples/schemas/super-refine'
+
+test('With JS enabled', async ({ example }) => {
+  const { button, page } = example
+  const planType = example.field('planType')
+  const quantity = example.field('quantity')
+
+  await page.goto(route)
+
+  // Render
+  await example.expectSelect(planType, { value: 'personal' })
+  await example.expectField(quantity, {
+    value: '',
+    required: true,
+  })
+
+  // fill
+  await quantity.input.fill('1')
+
+  // Submit
+  await expect(button).toBeEnabled()
+  await button.click()
+
+  await example.expectData({
+    planType: 'personal',
+    quantity: 1,
+  })
+})
