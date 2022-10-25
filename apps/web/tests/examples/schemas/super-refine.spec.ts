5498db5c6765046fd7978dc6a2c73eea3cc563ce
Diogo Biazus <diogo@biazus.ca>
2022-10-25 15:09:53 -0400

    Add test cases to refinement validation error

--- apps/web/tests/examples/schemas/super-refine.spec.ts
+++ apps/web/tests/examples/schemas/super-refine.spec.ts
@@ -55,3 +55,56 @@ testWithoutJS('With JS disabled', async ({ example }) => {
     quantity: 1,
   })
 })
+
+test('With JS enabled we see a refinement validation error', async ({
+  example,
+}) => {
+  const { button, page } = example
+  const planType = example.field('planType')
+  const quantity = example.field('quantity')
+
+  await page.goto(route)
+
+  // fill
+  await planType.input.selectOption('corporate')
+  await quantity.input.fill('1')
+
+  // Submit
+  await expect(button).toBeEnabled()
+  await button.click()
+
+  await example.expectError(
+    planType,
+    'For corporate cards you must issue at least 8',
+  )
+})
+
+testWithoutJS(
+  'With JS disabled we see a refinement validation error',
+  async ({ example }) => {
+    const { button, page } = example
+    const planType = example.field('planType')
+    const quantity = example.field('quantity')
+
+    await page.goto(route)
+
+    // Render
+    await example.expectSelect(planType, { value: 'personal' })
+    await example.expectField(quantity, {
+      value: '',
+      required: true,
+    })
+
+    // fill
+    await quantity.input.fill('1')
+
+    // Submit
+    await expect(button).toBeEnabled()
+    await button.click()
+
+    await example.expectData({
+      planType: 'personal',
+      quantity: 1,
+    })
+  },
+)
