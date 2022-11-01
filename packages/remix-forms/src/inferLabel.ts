82d133e72d1eac8a3ccecad11a52687987f75efb
Diogo Biazus <diogo@biazus.ca>
2022-11-01 12:08:53 -0400

    Move startCase function to inferLabel as private function since it is not used anywhere else

--- packages/remix-forms/src/inferLabel.ts
+++ packages/remix-forms/src/inferLabel.ts
@@ -1,4 +1,9 @@
-import { startCase } from './startCase'
+function startCase(str: string): string {
+  const matches = str.match(
+    /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g,
+  ) ?? ['']
+  return matches.map((x) => x.charAt(0).toUpperCase() + x.slice(1)).join(' ')
+}
 
 function inferLabel(fieldName: string) {
   return startCase(fieldName).replace(/Url/g, 'URL')
