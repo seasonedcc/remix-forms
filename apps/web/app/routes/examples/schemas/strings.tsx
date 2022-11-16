3b6205bdc7dbb230d075bf39156ba1b179082dad
Gustavo Guichard <gustavoguichard@gmail.com>
2022-11-16 12:25:16 -0300

    Fix phone regex example, the '\' was being hidden by the template string

--- apps/web/app/routes/examples/schemas/strings.tsx
+++ apps/web/app/routes/examples/schemas/strings.tsx
@@ -29,7 +29,7 @@ const code = `const schema = z.object({
   phoneNumber: z
     .string()
     .regex(
-      /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im,
+      /^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$/im,
       'Invalid phone number',
     ),
 })
