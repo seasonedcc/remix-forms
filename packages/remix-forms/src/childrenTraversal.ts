f936ecc4d6c839dc736293ed7bd330ba1195827c
Diogo Biazus <diogo@biazus.ca>
2022-12-05 19:05:35 -0500

    Add findElement and parent functions to find elements in the element tree so we can change label htmlFor based on the radio wrapper and radio sibbling. Also ensure that mapChildren preserves the object reference during the mapping so we can compare objects during the map.

--- packages/remix-forms/src/childrenTraversal.ts
+++ packages/remix-forms/src/childrenTraversal.ts
@@ -6,25 +6,54 @@ function mapChildren(
 ): React.ReactNode {
   return React.Children.map(children, (child) => {
     if (!React.isValidElement(child)) return child
+    const mapped = fn(child)
     if (child.props.children && typeof child.props.children !== 'function') {
-      return fn(
-        React.cloneElement(child, {
-          ...child.props,
-          children: mapChildren(child.props.children, fn),
-        }),
-      )
+      const mappedChildren = mapChildren(child.props.children, fn)
+      return mapped && React.cloneElement(mapped, { children: mappedChildren })
     }
-    return fn(child)
+    return mapped
+  })
+}
+
+function findElement(
+  root: React.ReactNode,
+  predicate: (element: React.ReactElement) => boolean,
+): React.ReactElement | null {
+  const initialState: React.ReactElement | null = null
+
+  return reduceElements<React.ReactElement | null>(
+    root,
+    initialState,
+    (previous, current) => {
+      if (previous) return previous
+
+      if (predicate(current)) {
+        return current
+      }
+      return null
+    },
+  )
+}
+
+function parent(
+  root: React.ReactNode,
+  child: React.ReactElement,
+): React.ReactElement | null {
+  return findElement(root, (parentCandidate) => {
+    const me = parentCandidate.props?.children?.find?.(
+      (ch: React.ReactNode) => {
+        return ch === child
+      },
+    )
+
+    return !!me
   })
 }
 
 function reduceElements<T>(
   children: React.ReactNode,
   initialState: T,
-  reducer: (
-    previousState: T,
-    currentState: React.ReactElement,
-  ) => T,
+  reducer: (previousState: T, currentState: React.ReactElement) => T,
 ): T {
   let foldedValue = initialState
   React.Children.forEach(children, (child) => {
@@ -34,9 +63,8 @@ function reduceElements<T>(
     if (child.props.children && typeof child.props.children !== 'function') {
       foldedValue = reduceElements(child.props.children, foldedValue, reducer)
     }
-
   })
   return foldedValue
 }
 
-export { mapChildren, reduceElements }
+export { mapChildren, reduceElements, findElement, parent }
