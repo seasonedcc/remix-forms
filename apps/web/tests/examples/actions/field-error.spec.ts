da2d681c98776214ae5505deced676ebec0c077c
Diogo Biazus <diogo@biazus.ca>
2022-12-12 17:30:02 -0500

    Add test case for client clearing error from server

--- apps/web/tests/examples/actions/field-error.spec.ts
+++ apps/web/tests/examples/actions/field-error.spec.ts
@@ -1,6 +1,33 @@
 import { test, testWithoutJS, expect } from 'tests/setup/tests'
 
 const route = '/examples/actions/field-error'
+test('With JS enabled clear server error on the client', async ({ example }) => {
+  const { email, password, button, page } = example
+
+  await page.goto(route)
+
+  // Render
+  await example.expectField(email)
+  await example.expectField(password)
+  await expect(button).toBeEnabled()
+
+  // Make generate server error
+  await email.input.fill('foo@bar.com')
+  await example.expectValid(email)
+  await password.input.fill('123456')
+  await example.expectValid(password)
+
+  // Submit form
+  button.click()
+  await expect(button).toBeDisabled()
+
+  // Show field error
+  await example.expectError(email, 'Email already taken')
+
+  // Submit valid form
+  await email.input.fill('john@doe.com')
+  await example.expectValid(email)
+})
 
 test('With JS enabled', async ({ example }) => {
   const { email, password, button, page } = example
