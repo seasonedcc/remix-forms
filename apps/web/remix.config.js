0782d19ab35f93896b1c501bf9fbbe612e7e472a
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-12-16 11:16:00 -0300

    Add infrastructure for mdx docs

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -8,4 +8,12 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
+  mdx: async (filename) => {
+    const [rehypeHighlight] = await Promise.all([
+      import('rehype-highlight').then((mod) => mod.default),
+    ])
+    return {
+      rehypePlugins: [rehypeHighlight],
+    }
+  },
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.white'),
+            h1: {
+              color: theme('colors.pink[500]'),
+              fontWeight: 'bold',
+            },
+            'h2, h3, h4, h5, h6': {
+              color: theme('colors.white')
+            }
+          },
+        },
+      }),
+    },
   },
-  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/typography'),
+  ],
 }
