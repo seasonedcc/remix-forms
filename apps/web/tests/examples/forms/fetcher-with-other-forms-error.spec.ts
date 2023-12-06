a0a252782144058d7c394b8958b7b49e9c2b6f42
Daniel Weinmann <danielweinmann@gmail.com>
2023-12-06 10:34:42 -0300

    Ensure action data errors are not shown in fetcher forms

---
+++ apps/web/tests/examples/forms/fetcher-with-other-forms-error.spec.ts
@@ -0,0 +1,49 @@
+import { test, testWithoutJS, expect } from 'tests/setup/tests'
+
+const route = '/test-examples/fetcher-with-other-forms-error'
+
+test('With JS enabled', async ({ example }) => {
+  const { page } = example
+  await page.goto(route)
+
+  const button = page.locator('#form button:has-text("OK"):visible')
+
+  // Render
+  await expect(button).toBeEnabled()
+
+  // Client-side validation
+  await button.click()
+
+  // Show field errors and focus on the first field
+
+  await expect(page.locator('#form > div[role="alert"]:visible')).toHaveText(
+    'This error should not show inside the fetcher form',
+  )
+
+  expect(
+    await page.locator('#fetcher-form > div[role="alert"]:visible').count(),
+  ).toEqual(0)
+})
+
+testWithoutJS('With JS disabled', async ({ example }) => {
+  const { page } = example
+  await page.goto(route)
+
+  const button = page.locator('#form button:has-text("OK"):visible')
+
+  // Render
+  await expect(button).toBeEnabled()
+
+  // Client-side validation
+  await button.click()
+
+  // Show field errors and focus on the first field
+
+  await expect(page.locator('#form > div[role="alert"]:visible')).toHaveText(
+    'This error should not show inside the fetcher form',
+  )
+
+  expect(
+    await page.locator('#fetcher-form > div[role="alert"]:visible').count(),
+  ).toEqual(0)
+})
