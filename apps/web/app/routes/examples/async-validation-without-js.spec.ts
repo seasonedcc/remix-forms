b3bc81653e51754985455a89f9ebdc3a3759b53e
Daniel Weinmann <danielweinmann@gmail.com>
2025-05-27 22:22:39 -0300

    Split slow e2e test files

---
+++ apps/web/app/routes/examples/async-validation-without-js.spec.ts
@@ -0,0 +1,53 @@
+import { testWithoutJS } from 'tests/setup/tests'
+
+const route = '/examples/forms/async-validation'
+
+testWithoutJS('With JS disabled', async ({ example }) => {
+  const { password, button, page } = example
+  const username = example.field('username')
+
+  await page.goto(route)
+
+  // Server-side validation
+  await button.click()
+  await page.reload()
+
+  // Show field errors and focus on the first field
+  await example.expectError(
+    username,
+    'String must contain at least 1 character(s)'
+  )
+
+  await example.expectError(
+    password,
+    'String must contain at least 1 character(s)'
+  )
+
+  await example.expectAutoFocus(username)
+  await example.expectNoAutoFocus(password)
+
+  // Make first field be valid, focus goes to the second field
+  await username.input.fill('john')
+  await button.click()
+  await page.reload()
+  await example.expectValid(username)
+  await example.expectAutoFocus(password)
+  await example.expectNoAutoFocus(username)
+
+  // Make form be valid
+  await username.input.fill('foo')
+  await password.input.fill('123456')
+
+  // Submit form
+  await button.click()
+  await page.reload()
+
+  // Show field error
+  await example.expectError(username, 'Already taken')
+
+  // Submit valid form
+  await username.input.fill('john')
+  await button.click()
+  await page.reload()
+  await example.expectData({ username: 'john', password: '123456' })
+})
